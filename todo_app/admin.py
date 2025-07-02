e1d6d3d10fd8e6f817b56694f259494c672569d0
Erbolot <eraabc.main@gmail.com>
2025-07-02 13:12:56 +0600

    настройка админки

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from  .models import Task
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status']
+    list_filter = ['status']
+    search_fields = ['title', 'description']
+    fields = ['title', 'description', 'status', 'finish_date']
+
+admin.site.register(Task,TaskAdmin)
