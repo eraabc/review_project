1492e6cb1a4ce28579762b4cff976724dbafa1bc
Erbolot <eraabc.main@gmail.com>
2025-07-02 13:03:13 +0600

    создание модели task и регмстрация в админке

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+class Task(models.Model):
+    title = models.CharField(max_length=100,verbose_name='Название задачи')
+    description = models.TextField(verbose_name='Описание задачи')
+    status = models.CharField(choices=status_choices, default=status_choices[0][0] , max_length=100,verbose_name='Выбор статуса')
+    finish_date = models.DateField(null=True, blank=True,verbose_name='Дата выполнения задачи')
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
