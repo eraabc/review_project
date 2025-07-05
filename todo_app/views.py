8416efaccbac064155bf98e292487d1bad034e4f
Erbolot <eraabc.main@gmail.com>
2025-07-05 22:37:52 +0600

    изменения в views изменил redirect и детального просмотра задачи

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from .models import  Task,status_choices
 
 def task_list(request):
@@ -13,27 +13,21 @@ def add_task(request):
         description = request.POST.get('description')
         status = request.POST.get('status')
         finish_date = request.POST.get('finish_date')
-        Task.objects.create(title=title,description=description,status=status,finish_date=finish_date)
-        return  HttpResponseRedirect('/')
+        task = Task.objects.create(title=title,description=description,status=status,finish_date=finish_date)
+        return redirect('task_detail',pk = task.pk)
     else:
         return render(request, 'add_task.html',{'status_choices':status_choices})
 
 
-def task_detail(request):
-    task_id = request.GET.get('id')
-    if task_id:
-        try:
-            task = Task.objects.get(id=task_id)
-            return render(request,'task_detail.html',{'task':task})
-        except Task.DoesNotExist:
-            return HttpResponseRedirect('/')
-    else:
-        return HttpResponseRedirect('/')
+def task_detail(request,**kwargs):
+    task_id = kwargs.get('pk')
+    task = get_object_or_404(Task,pk=task_id)
+    return render(request,'task_detail.html',{'task':task})
 
 
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     if request.method == "POST":
         task.delete()
-        return HttpResponseRedirect('/')
+        return redirect('task_list')
     return HttpResponseRedirect(f'/task_detail/?id={task_id}')
