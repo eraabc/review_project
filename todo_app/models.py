754510f523af16e844f3dc2db819eb483833fd80
Erbolot <eraabc.main@gmail.com>
2025-07-05 22:16:11 +0600

    Добавил поле в модельке extra_info

--- todo_app/models.py
+++ todo_app/models.py
@@ -6,6 +6,7 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 class Task(models.Model):
     title = models.CharField(max_length=100,verbose_name='Название задачи')
     description = models.TextField(verbose_name='Описание задачи')
+    extra_info = models.TextField(verbose_name='Дополнительное описание',null=True,blank=True)
     status = models.CharField(choices=status_choices, default=status_choices[0][0] , max_length=100,verbose_name='Выбор статуса')
     finish_date = models.DateField(null=True, blank=True,verbose_name='Дата выполнения задачи')
 
