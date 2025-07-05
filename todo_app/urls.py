889735ab661d8056fabe45d554a4528aeb611890
Erbolot <eraabc.main@gmail.com>
2025-07-05 22:19:42 +0600

    Изменил urls добавил имена и изменил путь детального просмотра

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path,include
 from todo_app.views import task_list,add_task,task_detail,delete_task
 
 urlpatterns = [
-    path('',task_list),
-    path('add-task/',add_task),
-    path('task/',task_detail),
+    path('',task_list,name='task_list'),
+    path('add-task/',add_task,name='add_task'),
+    path('task/<int:pk>',task_detail,name='task_detail'),
     path('tasks/delete/<int:task_id>/', delete_task, name='delete_task'),
 ]
