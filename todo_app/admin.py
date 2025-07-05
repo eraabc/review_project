28736661f19d5a159b6c4b688d016811541b59d3
Erbolot <eraabc.main@gmail.com>
2025-07-05 23:23:16 +0600

    добавление фикстуры

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -5,6 +5,5 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'status']
     list_filter = ['status']
     search_fields = ['title', 'description']
-    fields = ['title', 'description', 'status', 'finish_date']
 
 admin.site.register(Task,TaskAdmin)
