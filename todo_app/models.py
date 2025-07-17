a8fdd0dd723271af47cd6e012c8718607e90c16b
Erbolot <eraabc.main@gmail.com>
2025-07-18 01:49:22 +0600

    создал модели для типов и статусов также изменил модель Task

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,19 +1,50 @@
 from django.db import models
+from pytils.translit import slugify
 
-
-status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
 
 class Task(models.Model):
-    title = models.CharField(max_length=100,verbose_name='Название задачи')
-    description = models.TextField(verbose_name='Описание задачи')
-    extra_info = models.TextField(verbose_name='Дополнительное описание',null=True,blank=True)
-    status = models.CharField(choices=status_choices, default=status_choices[0][0] , max_length=100,verbose_name='Выбор статуса')
-    finish_date = models.DateField(null=True, blank=True,verbose_name='Дата выполнения задачи')
+    title = models.CharField(max_length=100,verbose_name='Название задачи',unique=True)
+    description = models.TextField(verbose_name='Описание',null=True,blank=True)
+    status = models.ForeignKey('todo_app.Status',on_delete=models.PROTECT,verbose_name='Статус',related_name='tasks')
+    type = models.ForeignKey('todo_app.Type',on_delete=models.PROTECT,verbose_name='Тип',related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True,verbose_name='Время обновления')
+    slug = models.SlugField(max_length=100,verbose_name='Слаг',blank=True,null=True,unique=True)
+
+
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args,**kwargs)
+
 
     def __str__(self):
-        return f"{self.id} - {self.title}"
+        return f"{self.title} - {self.status.title}"
 
     class Meta:
         db_table = 'tasks'
-        verbose_name = 'Task'
-        verbose_name_plural = 'Tasks'
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
+class Status(models.Model):
+    title = models.CharField(max_length=100,verbose_name='Статус',unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'statuses'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Type(models.Model):
+    title = models.CharField(max_length=100,verbose_name='Статус',unique=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'types'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
